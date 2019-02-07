82365fb7d7af4e85b0be8b0a2d9b62d96d4628ac
Tural Mehralı <turalmehrali@gmail.com>
2019-02-07 23:40:38 +0400

    Даны два целых числа. Программа должна вывести единицу, если первое число больше второго, двойку, если второе больше первого, или ноль, если они равны

---
+++ Введение в программирование (C++)/1.4 Условный оператор/02. Даны два целых числа. Программа должна вывести единицу, если первое число больше второго, двойку, если второе больше первого, или ноль, если они равны.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a, b;
+    cin >> a;
+    cin >> b;
+    if(a > b){
+        cout << "1";
+    }
+    if(a < b){
+        cout << "2";
+    }
+    if(a == b){
+        cout << "0";
+    }
+  return 0;
+}
